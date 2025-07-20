63a2940c5dd0b653878cc31dfac314da8df11402
AliMusa02 <ali.musayev044@gmail.com>
2025-07-20 15:09:59 +0000

    debud is setted false

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--xm(k%9h!dk^)95=uwlai2@(z71hq0ujv0k6$&w&9oet5y628e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
@@ -168,14 +168,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # STATIC_URL = '/static/'
 
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 # MEDIA_URL = '/media/'
 
