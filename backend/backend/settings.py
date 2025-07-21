8cdcae82990c3f6d3f74e96e849c61545c9ba9c8
AliMusa02 <ali.musayev044@gmail.com>
2025-07-21 17:58:37 +0000

    media url and static url done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,7 +32,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
-    "https://teamplaybackend.up.railway.app", "http://127.0.0.1:8000", "localhost", "http://localhost:5173"]
+    "https://teamplaybackend.up.railway.app", "http://127.0.0.1:8000", "http://localhost:5173"]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
