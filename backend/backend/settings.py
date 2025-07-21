a23dd764b7bb81e227079acec56586d9e15ccba9
AliMusa02 <ali.musayev044@gmail.com>
2025-07-21 18:04:17 +0000

    media url and static url done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -169,15 +169,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_URL = '/static/'
 
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 MEDIA_URL = '/media/'
-
-# MEDIA_URL = '/media/'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
