1930af2bd6f3bfccb54ecc8952137783fe036505
AliMusa02 <ali.musayev044@gmail.com>
2025-07-22 02:07:35 +0000

    posts admin added

--- backend/posts/admin.py
+++ backend/posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Posts
 # Register your models here.
+
+
+@admin.register(Posts)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'content',
+                    'post_pic', 'created_at')
+    search_fields = ('author')
+    ordering = ('created_at')
+
+
+# @admin.register(TeamMember)
+# class VenueSlotsAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'user', 'team', 'joined_at', 'role')
+#     list_filter = ('team', 'user')
