2b2f178d68ab61f49266e9f2dd4bee8f84f5d27d
AliMusa02 <ali.musayev044@gmail.com>
2025-07-22 01:46:28 +0000

    teams admin.py modified

--- backend/teams/admin.py
+++ backend/teams/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Teams, TeamMember
 # Register your models here.
+
+
+@admin.register(Teams)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('id', 'team_name', 'team_logo',
+                    'created_at', 'about', 'captain')
+    search_fields = ('team_name')
+    ordering = ('created_at',)
+
+
+@admin.register(TeamMember)
+class VenueSlotsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'team', 'joined_at', 'role')
+    list_filter = ('team', 'user')
