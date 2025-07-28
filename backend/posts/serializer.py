f7b076debe16b7ae611f65eeb60785552d24c0db
AliMusa02 <ali.musayev044@gmail.com>
2025-07-28 11:53:08 +0000

    posts response team id addedd

--- backend/posts/serializer.py
+++ backend/posts/serializer.py
@@ -15,18 +15,26 @@ class PostSerializer(serializers.ModelSerializer):
     # author_team_name = serializers.CharField(
     #     source='author.team.team_name', null=True, blank=True, read_only=True)
     author_team_name = serializers.SerializerMethodField()
+    author_team_id = serializers.SerializerMethodField()
 
     class Meta:
         model = Posts
         fields = ["id", "content", "created_at", "post_pic", "author_id",
-                  'author_username', 'author_profilePic', 'author_team_name']
+                  'author_username', 'author_profilePic', 'author_team_name', 'author_team_id']
         # extra_kwargs = {"author_username": {"read_only": True}, "author_profilePic": {
         #     "read_only": True}, "author_team_name": {"read_only": True}}
 
     def get_author_team_name(self, obj):
         try:
+            # print(obj.author.team)
             return obj.author.team.team_name
         except AttributeError:
             return "User has no team"
         # except Exception:
         #     return None
+
+    def get_author_team_id(self, obj):
+        try:
+            return obj.author.team.id
+        except AttributeError:
+            return None
